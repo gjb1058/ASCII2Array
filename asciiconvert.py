58d7cda816c70c7193ccf3dc025e78c322b49c3a
gjb1058 <gjb1058@rit.edu>
2019-09-17 15:58:38 -0400

    Properly cut off extra comma. Now dynamic, can take different sized lines.

--- asciiconvert.py
+++ asciiconvert.py
@@ -26,12 +26,22 @@ if os.path.exists(sys.argv[1]):
         cols = cols-1
         command = "char art["+str(rows)+"]["+str(cols)+"] = { \n "
         print(command)
+        count = 0
         for line in lines:
             command1="{ "
             for char in line:
                 if char != '\n':
                     command1+="'"+str(char)+"' , "
+                    count+=1
+                else:
+                    if count < cols:
+                        while(count<cols):
+                            command1+="' ' , "
+                            count+=1
+            
+            command1 = command1[:-3]
             command1+="} ,\n"
+            count = 0
             print(command1)
     except Exception as e:
         print(e)
